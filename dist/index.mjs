d554ab52ba436066db71c345a8cd7aa364e48c5e
Aykut Akgün <aykut.akguen@outlook.de>
2023-03-21 10:36:03 +0100

    temp

---
+++ dist/index.mjs
@@ -0,0 +1,134 @@
+import puppeteer from "puppeteer";
+import pkg from "puppeteer-autoscroll-down";
+import sharp from "sharp";
+import fs from "fs";
+import { exit } from "process";
+const { scrollPageToBottom, scrollPageToTop } = pkg;
+if (process.argv[2] === undefined) {
+    console.log("ERROR, NO IMG URL GIVEN, ADD IT TO THE END"), console.log();
+    exit(1);
+}
+function sleep(milliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+async function createImagesV1() {
+    try {
+        //
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1000 });
+        await page.goto(process.argv[2], { waitUntil: "networkidle0" });
+        //@ts-ignore
+        const heightTemp = await page.evaluate((e) => {
+            const h = document.body.getBoundingClientRect().height;
+            const heightApp = document.body.children[0].getBoundingClientRect().height;
+            if (h > heightApp)
+                return h;
+            return heightApp;
+        });
+        await page.setViewport({
+            width: 1935,
+            height: Math.floor(heightTemp) + 20000,
+        });
+        await sleep(3000);
+        await page.screenshot({ path: "temp.png", fullPage: true });
+        await sleep(200);
+        await page.setViewport({ width: 1920, height: 1080 });
+        await sleep(1000);
+        //@ts-ignore
+        const host = await page.evaluate((e) => {
+            return window.location.host;
+        });
+        //@ts-ignore
+        const height = await page.evaluate((e) => {
+            const h = document.body.getBoundingClientRect().height;
+            const heightApp = document.body.children[0].getBoundingClientRect().height;
+            if (h > heightApp)
+                return h;
+            return heightApp;
+        });
+        await browser.close();
+        //@ts-ignore
+        console.log(Math.floor(height));
+        await sharp("./temp.png")
+            .extract({ width: 1920, height: Math.floor(height), left: 0, top: 0 })
+            .toFile("final.png")
+            .then(function (new_file_info) {
+            console.log("Image cropped and saved");
+        })
+            .catch(function (err) {
+            console.log(err);
+        });
+        await sleep(500);
+        await sharp("./final.png")
+            .resize({ width: 1440 })
+            .jpeg({ quality: 80 })
+            .toFile(host + "-compressed-v1.jpeg");
+        await sharp("./final.png")
+            .resize({ width: 800 })
+            .extract({ width: 800, height: 550, top: 0, left: 0 })
+            .jpeg({ quality: 80 })
+            .toFile(host + "-thumbnail-v1.jpeg");
+        fs.renameSync("./final.png", "./" + host + "-full-v1.png");
+        fs.unlinkSync("./temp.png");
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+async function createImagesV2() {
+    try {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.goto(process.argv[2], { waitUntil: "networkidle0" });
+        await sleep(3000);
+        //@ts-ignore
+        await scrollPageToBottom(page, {
+            size: 50,
+            delay: 250,
+        });
+        await page.screenshot({ path: "temp2.png", fullPage: true });
+        await sleep(200);
+        //@ts-ignore
+        const host = await page.evaluate((e) => {
+            return window.location.host;
+        });
+        //@ts-ignore
+        const height = await page.evaluate((e) => {
+            const h = document.body.getBoundingClientRect().height;
+            const heightApp = document.body.children[0].getBoundingClientRect().height;
+            if (h > heightApp)
+                return h;
+            return heightApp;
+        });
+        await browser.close();
+        await sharp("./temp2.png")
+            .extract({ width: 1920, height: Math.floor(height), left: 0, top: 0 })
+            .toFile("final2.png")
+            .then(function (new_file_info) {
+            console.log("Image cropped and saved");
+        })
+            .catch(function (err) {
+            console.log(err);
+        });
+        await sleep(500);
+        await sharp("./final2.png")
+            .resize({ width: 1440 })
+            .jpeg({ quality: 80 })
+            .toFile(host + "-compressed-v2.jpeg");
+        await sharp("./final2.png")
+            .resize({ width: 800 })
+            .extract({ width: 800, height: 550, top: 0, left: 0 })
+            .jpeg({ quality: 80 })
+            .toFile(host + "-thumbnail-v2.jpeg");
+        fs.renameSync("./final2.png", "./" + host + "-full-v2.png");
+        fs.unlinkSync("./temp2.png");
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+createImagesV1();
+createImagesV2();
+//# sourceMappingURL=index.mjs.map
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import pkg from "puppeteer-autoscroll-down";
 import sharp from "sharp";
 import fs from "fs";
 import { exit } from "process";
+const { scrollPageToBottom, scrollPageToTop } = pkg;
 
 if (process.argv[2] === undefined) {
   console.log("ERROR, NO IMG URL GIVEN, ADD IT TO THE END"), console.log();
@@ -13,64 +14,127 @@ function sleep(milliseconds) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
-async function startScreenshotting() {
-  //
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+async function createImagesV1() {
+  try {
+    //
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  await page.setViewport({ width: 1920, height: 5000 });
+    await page.setViewport({ width: 1920, height: 1000 });
 
-  await page.goto(process.argv[2], { waitUntil: "networkidle0" });
-  const heightTemp = await page.evaluate((e) => {
-    const h = document.body.getBoundingClientRect().height;
-    const heightApp = document.body.children[0].getBoundingClientRect().height;
-    if (h > heightApp) return h;
-    return heightApp;
-  });
-  await page.setViewport({
-    width: 1920,
-    height: Math.floor(heightTemp) + 20000,
-  });
-  await sleep(7000);
-  await page.screenshot({ path: "temp.png", fullPage: true });
-  await sleep(200);
-  await page.setViewport({ width: 1920, height: 1080 });
-  await sleep(1000);
-
-  const host = await page.evaluate((e) => {
-    return window.location.host;
-  });
-
-  const height = await page.evaluate((e) => {
-    const h = document.body.getBoundingClientRect().height;
-    const heightApp = document.body.children[0].getBoundingClientRect().height;
-    if (h > heightApp) return h;
-    return heightApp;
-  });
-
-  await browser.close();
-  await sharp("./temp.png")
-    .extract({ width: 1920, height: Math.floor(height), left: 0, top: 0 })
-    .toFile("final.png")
-    .then(function (new_file_info) {
-      console.log("Image cropped and saved");
-    })
-    .catch(function (err) {
-      console.log(err);
+    await page.goto(process.argv[2], { waitUntil: "networkidle0" });
+    const heightTemp = await page.evaluate((e) => {
+      const h = document.body.getBoundingClientRect().height;
+      const heightApp =
+        document.body.children[0].getBoundingClientRect().height;
+      if (h > heightApp) return h;
+      return heightApp;
     });
-  await sleep(500);
+    await page.setViewport({
+      width: 1935,
+      height: Math.floor(heightTemp) + 20000,
+    });
+    await sleep(3000);
+    await page.screenshot({ path: "temp.png", fullPage: true });
+    await sleep(200);
+    await page.setViewport({ width: 1920, height: 1080 });
+    await sleep(1000);
 
-  await sharp("./final.png")
-    .resize({ width: 1440 })
-    .jpeg({ quality: 80 })
-    .toFile(host + "-compressed.jpeg");
-  await sharp("./final.png")
-    .resize({ width: 800 })
-    .extract({ width: 800, height: 550, top: 0, left: 0 })
-    .jpeg({ quality: 80 })
-    .toFile(host + "-thumbnail.jpeg");
-  fs.renameSync("./final.png", "./" + host + "-full.png");
-  fs.unlinkSync("./temp.png");
+    const host = await page.evaluate((e) => {
+      return window.location.host;
+    });
+
+    const height = await page.evaluate((e) => {
+      const h = document.body.getBoundingClientRect().height;
+      const heightApp =
+        document.body.children[0].getBoundingClientRect().height;
+      if (h > heightApp) return h;
+      return heightApp;
+    });
+
+    await browser.close();
+
+    await sharp("./temp.png")
+      .extract({ width: 1920, height: Math.floor(height), left: 0, top: 0 })
+      .toFile("final.png")
+      .then(function (new_file_info) {
+        console.log("Image cropped and saved");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    await sleep(500);
+
+    await sharp("./final.png")
+      .resize({ width: 1440 })
+      .jpeg({ quality: 80 })
+      .toFile(host + "-compressed-v1.jpeg");
+    await sharp("./final.png")
+      .resize({ width: 800 })
+      .extract({ width: 800, height: 550, top: 0, left: 0 })
+      .jpeg({ quality: 80 })
+      .toFile(host + "-thumbnail-v1.jpeg");
+    fs.renameSync("./final.png", "./" + host + "-full-v1.png");
+    fs.unlinkSync("./temp.png");
+
+    console.log("variante 1 was sucessfull");
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-startScreenshotting();
+async function createImagesV2() {
+  try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.setViewport({ width: 1920, height: 1080 });
+
+    await page.goto(process.argv[2], { waitUntil: "networkidle0" });
+
+    await sleep(3000);
+
+    //@ts-ignore
+    await scrollPageToBottom(page, {
+      size: 50,
+      delay: 250,
+    });
+
+    await page.screenshot({ path: "final2.png", fullPage: true });
+    await sleep(200);
+
+    //@ts-ignore
+    const host = await page.evaluate((e) => {
+      return window.location.host;
+    });
+
+    //@ts-ignore
+    const height = await page.evaluate((e) => {
+      const h = document.body.getBoundingClientRect().height;
+      const heightApp =
+        document.body.children[0].getBoundingClientRect().height;
+      if (h > heightApp) return h;
+      return heightApp;
+    });
+
+    await browser.close();
+    await sleep(300);
+
+    await sharp("./final2.png")
+      .resize({ width: 1440 })
+      .jpeg({ quality: 80 })
+      .toFile(host + "-compressed-v2.jpeg");
+    await sharp("./final2.png")
+      .resize({ width: 800 })
+      .extract({ width: 800, height: 550, top: 0, left: 0 })
+      .jpeg({ quality: 80 })
+      .toFile(host + "-thumbnail-v2.jpeg");
+    fs.renameSync("./final2.png", "./" + host + "-full-v2.png");
+    console.log("variante 2 was sucessfull");
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+createImagesV1();
+createImagesV2();
